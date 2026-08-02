88eb1876653ab4b3850782b09a49ab19bd9b72e5
Volodymyr Butenko <voxa23ne@gmail.com>
2026-08-02 22:20:10 +0300

    fix

--- public/sw.js
+++ public/sw.js
@@ -130,7 +130,6 @@ self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE)),
   );
-  self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
